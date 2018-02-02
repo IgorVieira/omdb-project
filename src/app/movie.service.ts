ce80074bfb04c229bc6d0d74b376b1f1122a4c4b
Igor Vieira <igor.p.r.vieira@gmail.com>
2018-02-02 16:25:09 -0200

    add function for get movie by id

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -24,4 +24,33 @@ export class MovieService {
     }))
   }
 
+  getMovieById(movie_id: string) {
+    return this.http.get(`${this.baseUrl}/?i=${movie_id}${this.hashUrl}`)
+    .toPromise()
+    .then(res => res.json())
+    .then(movie => {
+      const {
+        Title, Year, Runtime, Genre, Director,
+        Writer, Actors, Plot, Language, Country,
+        Awards, Poster, Production, Website, imdbID
+      } = movie
+      return {
+        id: imdbID,
+        title: Title,
+        year: Year,
+        runtime: Runtime,
+        genre: Genre,
+        director: Director,
+        writer: Writer,
+        actors: Actors,
+        plot: Plot,
+        language: Language,
+        country: Country,
+        awards: Awards,
+        poster: Poster,
+        production: Production,
+        website: Website
+      }
+    })
+  }
 }
