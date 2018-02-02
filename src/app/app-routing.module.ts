535ee1a83d2a0b8933054eb27fb3a00a89c2ea69
Igor Vieira <igor.p.r.vieira@gmail.com>
2018-02-02 16:25:27 -0200

    create  another router for get movie by id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { MovieComponent }      from './movie/movie.component';
+import { MovieDetailComponent }  from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'detail/:id', component: MovieDetailComponent },
   { path: 'movies', component: MovieComponent }
 ];
 
